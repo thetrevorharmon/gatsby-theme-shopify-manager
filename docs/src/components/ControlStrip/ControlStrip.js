4e6bf24fdf3e62a04de9e3b9ddc6670d644737f6
Trevor Harmon <9555730+thetrevorharmon@users.noreply.github.com>
2020-03-26 17:18:53 -0700

    [Docs] Added ControlStrip (#39)

---
+++ docs/src/components/ControlStrip/ControlStrip.js
@@ -0,0 +1,99 @@
+/** @jsx jsx */
+import {Button} from 'theme-ui';
+import {
+  useAddItemToCart,
+  useCartItems,
+  useRemoveItemFromCart,
+  useRemoveItemsFromCart,
+} from 'gatsby-theme-shopify-manager';
+import {useProducts} from '../../utils';
+import {jsx} from 'theme-ui';
+import {Box} from 'theme-ui';
+
+export function ControlStrip() {
+  return (
+    <Box sx={{padding: 3, pb: 3, background: '#ccc'}}>
+      <AddItemButton />
+      <RemoveItemButton />
+      <EmptyCartButton />
+    </Box>
+  );
+}
+
+export function AddItemButton() {
+  const addItemToCart = useAddItemToCart();
+  const products = useProducts();
+
+  async function addToCart() {
+    const productIndex = Math.floor(
+      Math.random() * Math.floor(products.length),
+    );
+    const variantId = products[productIndex].variants[0].shopifyId;
+    const quantity = 1;
+
+    try {
+      await addItemToCart(variantId, quantity);
+      alert('Successfully added an item to your cart!');
+    } catch {
+      alert('There was a problem adding an item to your cart.');
+    }
+  }
+
+  return (
+    <Button variant="controlStrip" onClick={addToCart}>
+      Add Item
+    </Button>
+  );
+}
+
+export function RemoveItemButton() {
+  const removeItemFromCart = useRemoveItemFromCart();
+  const cartItems = useCartItems();
+
+  async function removeFromCart() {
+    if (cartItems.length < 1) {
+      return;
+    }
+
+    const [cartItemToRemove] = cartItems;
+    console.log(cartItemToRemove);
+
+    try {
+      await removeItemFromCart(cartItemToRemove.variant.id);
+      alert('Successfully removed an item from your cart!');
+    } catch {
+      alert('There was a problem removing an item from your cart.');
+    }
+  }
+
+  return (
+    <Button sx={{mx: 2}} variant="controlStrip" onClick={removeFromCart}>
+      Remove Item
+    </Button>
+  );
+}
+
+export function EmptyCartButton() {
+  const removeItemsFromCart = useRemoveItemsFromCart();
+  const cartItems = useCartItems();
+
+  async function emptyCart() {
+    if (cartItems.length < 1) {
+      return;
+    }
+
+    try {
+      const variantIds = cartItems.map((cartItem) => cartItem.variant.id);
+      await removeItemsFromCart(variantIds);
+      alert('Successfully removed all items from your cart!');
+    } catch {
+      alert('There was a problem removing an item from your cart.');
+    }
+  }
+
+  return (
+    <Button variant="controlStrip" onClick={emptyCart}>
+      Empty Cart
+    </Button>
+  );
+}
